b6619dc5fdffa307abba0e17ec976acf0ae796bd
oguken <gura.time@icloud.com>
2021-05-01 18:19:56 +0900

    商品一覧表示機能1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  def index
-     #@items = Item.all
-  end
-
   def new
     @item = Item.new
   end
@@ -17,6 +13,10 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
+  def index
+    @items = Item.all
+ end
 
   private
   def item_params
