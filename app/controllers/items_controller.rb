0057a117b919c87f35d9e904bf6bc90dc884a41b
oguken <gura.time@icloud.com>
2021-05-03 16:11:29 +0900

    商品情報編集機能2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit] 
 
   def new
     @item = Item.new
@@ -23,7 +24,9 @@ class ItemsController < ApplicationController
  end
 
  def edit
-  @item = Item.find(params[:id])
+  #if current_user.id == @item.usergned_in? #出品者ではない 
+    #redirect_to action: :index #トップページ
+  #end
 end
 
 def update
@@ -35,8 +38,16 @@ def update
   end
 end
 
-  private
-  def item_params
-    params.require(:item).permit(:item_image, :item_name, :item_manual, :category_id, :item_condition_id, :send_money_burden_id, :send_area_id, :send_days_id, :price).merge(user_id: current_user.id)
+private
+def item_params
+  params.require(:item).permit(:item_image, :item_name, :item_manual, :category_id, :item_condition_id, :send_money_burden_id, :send_area_id, :send_days_id, :price).merge(user_id: current_user.id)
+end
+
+def move_to_index
+  @item = Item.find(params[:id])
+    unless current_user.id == @item.user.id 
+      redirect_to root_path
+    end
   end
 end
+
